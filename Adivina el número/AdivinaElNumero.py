a5e21363ae7dab3d976595032342862aeecb520b
MSPSA0 <mateotcf@gmail.com>
2023-11-05 20:43:04 -0300

    Adivina el número

---
+++ Adivina el número/AdivinaElNumero.py
@@ -0,0 +1,24 @@
+import random
+
+
+numeroSecreto: int = random.randint(0, 20)
+numeroIngresado: int
+intentos: int = 6
+
+while (True):
+	print("Ingrese el número")
+	numeroIngresado = int(input())
+	
+	if (numeroIngresado == numeroSecreto):
+		print("El número era", numeroIngresado)
+		break
+	elif (numeroIngresado > numeroSecreto):
+		print("El número es más chico")
+	else:
+		print("El número es más grande")
+	
+	intentos -= 1
+	if (intentos <= 0):
+		print("Ya no hay más intentos")
+		print("En número era", numeroSecreto)
+		break
