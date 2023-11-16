e52a62d126e58ab48dbbedc3a0a860f1e7e3a214
MSPSA0 <mateotcf@gmail.com>
2023-11-16 08:22:34 -0300

    Función cifrar

--- Cifrado César/CifradoCesar.py
+++ Cifrado César/CifradoCesar.py
@@ -8,20 +8,24 @@ palabra: str
 palabraCifrada: str
 desplazamiento: int
 
+def cifrar(texto: str, desplazamiento: int) -> str:
+    textoCifrado: str = ""
+    for c in texto:
+        indice = ABECEDARIO.find(c.upper())
+        if (indice >= 0):
+            esMinuscula = c.islower()
+            c = ABECEDARIO[indice + desplazamiento]
+            if (esMinuscula):
+                c = c.lower()
+        textoCifrado += c
+    return textoCifrado
+
 print("Ingrese la palabra")
 palabra = input()
 
 print("Ingrese el desplazamiento")
 desplazamiento = int(input())
 
-palabraCifrada = ""
-for c in palabra:
-    indice = ABECEDARIO.find(c.upper())
-    if (indice >= 0):
-        esMinuscula = c.islower()
-        c = ABECEDARIO[indice + desplazamiento]
-        if (esMinuscula):
-            c = c.lower()
-    palabraCifrada += c
+palabraCifrada = cifrar(palabra, desplazamiento)
 
 print(palabraCifrada)
